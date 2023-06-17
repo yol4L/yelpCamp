eae953d24941bad29fb3fb6c8b8fd43de4d72d8e
yola_liang <100056224+yol4L@users.noreply.github.com>
2023-06-18 00:00:44 +1000

    Adding basic map to the campground show page.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,7 @@
 const Campground = require("../models/campground");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapboxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapboxToken });
 const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
@@ -12,7 +15,16 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.createCampground = async (req, res, next) => {
   // console.log(req.body, req.files);
+  //Adding maps
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send();
+  //create a campground
   const campground = new Campground(req.body.campground);
+  campground.geometry = geoData.body.features[0].geometry;
   campground.images = req.files.map((file) => ({
     url: file.path,
     filename: file.filename,
@@ -20,6 +32,7 @@ module.exports.createCampground = async (req, res, next) => {
   campground.author = req.user._id;
   await campground.save();
   // console.log(campground);
+
   req.flash("success", "Successfully made a new compground!");
   res.redirect(`/campgrounds/${campground._id}`);
 };
